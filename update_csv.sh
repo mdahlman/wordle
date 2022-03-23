235df953c9a56ba5511c0d4b18109416c64f3eac
mdahlman <matt.dahlman+github@gmail.com>
2022-03-23 13:49:37 -0700

    tweak to shell script

--- update_csv.sh
+++ update_csv.sh
@@ -1,2 +1,16 @@
 # This command assumes connectivity to the relevant database will be automatic.
-psql -d postgres --csv -c "select the_date as \"Date\", word as \"Word\", comments as \"Comments\" from v_wordle order by the_date desc, comments ;" > wordle-answers.csv
+FILENAME=/Users/mdahlman/github/wordle/docs/_data/wordle_answers.csv
+psql -d postgres --csv -c "select the_date as \"Date\", word as \"Word\", comments as \"Comments\" from v_wordle order by the_date desc, comments ;" > $FILENAME
+
+cd /Users/mdahlman/github/wordle
+gco gh-pages
+git add $FILENAME
+git commit -m "answers through `date -v-1d +%Y-%m-%d`"
+git push origin $(current_branch)
+
+
+# Related notes
+#
+# Remember to launch Jekyll server locally like this:
+# cd /Users/mdahlman/github/wordle/docs
+# bundle exec jekyll serve --baseurl ''
